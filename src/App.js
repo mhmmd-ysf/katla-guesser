de64a3f93a66b29e15cd9044d0d27143accfa127
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2022-01-31 15:47:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -56,6 +56,12 @@ function App() {
   } else {
     kamus = []
   }
+  
+  function submitHandler(e) {
+    if(e.key === "Enter") {
+      e.target.blur()
+    }
+  }
 
   return (
     <div className="App">
@@ -115,10 +121,11 @@ function App() {
         </div>
       </div>
       <div>
-        <button className="bg-blue-400 rounded px-5 py-2 md:text-lg">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <button className="bg-blue-400 rounded px-5 py-2 md:text-lg" onClick={submitHandler}>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg> Cari
+          </svg> */}
+          Cari
         </button>
       </div>
       {
@@ -127,7 +134,7 @@ function App() {
           Loading...
         </div>
         :
-        <div className="grid grid-cols-5 md:grid-cols-10">
+        <div className="grid grid-cols-4 md:grid-cols-10">
           {kamus
           .map(item => {
             return (
